3e53b640fdf4505b343484244be1f2f8f4aab661
Stephen Bryant <stephen@stephenbryant.net>
2012-05-08 13:28:12 -0400

    Various additional cleanup and conversion.

--- twistedBot.py
+++ twistedBot.py
@@ -16,122 +16,118 @@ import json
 import cStringIO
 from twisted.internet import reactor
 
-class twistedBot(irc.IRCClient):
-	def __init__(self):
+class twistedBot( irc.IRCClient ):
+	def __init__( self ):
 		lastMsg = ""
 
-	def _get_nickname(self):
+	def _get_nickname( self ):
 		return self.factory.nickname
-	nickname = property(_get_nickname)
 
-	def signedOn(self):
-		self.join(self.factory.channel)
-		print "Signed on as "+self.nickname
+	nickname = property( _get_nickname )
+
+	def signedOn( self ):
+		self.join( self.factory.channel )
+		print "Signed on as " + self.nickname
 
 	def joined(self, channel):
-		print "Joined "+channel
+		print "Joined " + channel
 
-	def privmsg(self, user, channel, msg):
-		#print ("%s %s %s")%(user, channel, msg)
-		if user.endswith( "bot" ):
-			return
-		if not user:
+	def privmsg( self, user, channel, msg ):
+		if not user or user.endswith( "bot" ):
 			return
 		
-		newmsg = ""
-		prefix = ""
-
-		if self.nickname in msg:
-			msg = re.compile(self.nickname+"[:,]* ?", re.I).sub("", msg)
-			prefix = "%s: "%(user.split('!', 1)[0],)
-			extras = self.parseMsg(msg)
-			if extras == "": 
-				newmsg = self.getsentence(msg)
-			else:
-				newmsg = extras
-		elif "<3" in msg:
-			msg = re.compile("<3"+"[:,]* ?", re.I).sub("", msg)
-			prefix = "%s: "%(user.split('!', 1)[0],)
-			extras = self.parseMsg(msg)
-			if extras == "":
-				newmsg = self.getsentence(msg)
-			else:
-				newmsg = extras
-		elif channel == self.nickname:
-			extras = self.parseMsg(msg)
-			if extras == "":
-				newmsg = self.getsentence(msg)
-			else:
-				newmsg = extras
-		elif random.random() <= self.factory.chattiness:
-			msg = re.compile(user.split('!',1)[0]+"[:,]* ?", re.I).sub("", msg)
-			newmsg = self.getsentence(msg)
+		isPrivateMsg = channel == self.nickname
+		msgFrom = user.split( '!', 1 )[0]
+		replyTo = channel
+		replyMsg = ""
 		
-		self.lastMsg = newmsg
-		
-		if channel == self.nickname:
-			self.msg(user.split('!', 1)[0], prefix+newmsg)
-		elif newmsg != "":
-			self.msg(self.factory.channel, prefix+newmsg)
+		if isPrivateMsg:
+			msg = msg.split( " " )[1:].join( " " )
+			replyTo = msgFrom
+		else:
+			replyMsg = "%s: " % ( msgFrom, )
 
-	def parseMsg(self, msg): 
-		base = "http://twitter.com/anglebracket3/status/"
-		if "twitlast" in msg:
-			if hasattr(self, 'lastMsg') and self.lastMsg != "":
-				print self.lastMsg
-				status = self.factory.api.PostUpdate(self.lastMsg)
-			else:
+		msg = msg.strip()
+		reply = self.parseMsg( msg )
+
+		if reply == "" and ( msg.startswith( self.nickname ) or isPrivateMsg or random.random() <= self.factory.chattiness ): 
+			reply = self.getsentence( msg )
+
+		if reply != "":
+			replyMsg += reply
+			self.lastMsg = reply
+			self.msg( replyTo, replyMsg )
+
+	def parseMsg( self, msg ): 
+
+		if msg.startswith( "twitlast" ):
+			if not hasattr(self, 'lastMsg') or self.lastMsg != "":
 				return ""
-			return base + str(status.id)
-		elif "kittify" in msg:
+
+			print self.lastMsg
+			status = self.factory.api.PostUpdate( self.lastMsg )
+			return base + str( status.id )
+
+		elif msg.startswith( "kittify" ):
 			return self.kittify()
-		elif "kitlast" in msg:
+
+		elif msg.startswith( "kitlast" ):
 			url = self.kittify()
-			status = self.factory.api.PostUpdate(url)
-			return base+str(status.id)+" "+url
-		elif "source" in msg:
-			return "https://github.com/clholgat/twistedBot"
-		elif "help" in msg or "wat" in msg:
+			status = self.factory.api.PostUpdate( url )
+			return self.base + str( status.id ) + " " + url
+
+		elif msg.startswith( "source" ):
+			return self.config.get( "General", "source_url" )
+
+		elif msg.startswith( "help" ):
 			return "Commands: twitlast - tweets the last message; source - link to bot's source; kittify - kittenify the last message; kitlast - kittenify and post image to twitter; help - this message"
+
 		else:
 			return ""
 
-	def kittify(self):
-		if not hasattr(self, 'lastMsg') or self.lastMsg == "":
+	def kittify( self ):
+		if not hasattr( self, 'lastMsg' ) or self.lastMsg == "":
 			return ""
+
 		print self.lastMsg
-		kitten = "kitten%d.jpg"%random.choice(range(1,10))
-		im = Image.open("kittens/"+kitten)
+
+		kitten = "kitten%d.jpg" % random.choice( range( 1, 10 ) )
+		im = Image.open( "kittens/" + kitten )
 		box = im.getbbox()
 		width = box[2]
 		height = box[3]
 		fsize = 70
-		lines = textwrap.wrap(self.lastMsg, int(width/fsize*1.65))
+		lines = textwrap.wrap( self.lastMsg, int( width / fsize * 1.65 ) )
+
 		while len(lines) > 2:
 			fsize -= 2
-			lines = textwrap.wrap(self.lastMsg, int(width/fsize*1.65))
+			lines = textwrap.wrap( self.lastMsg, int( width / fsize * 1.65 ) )
 
-		draw = ImageDraw.Draw(im)
-		font = ImageFont.truetype("Arial.ttf", fsize)
-		draw.text((10, 10), lines[0], font=font, fill="white")
-		if len(lines) > 1:
-			draw.text((10, height-10-fsize), lines[-1], font=font)
-		im.save("tmp.jpg")
+		draw = ImageDraw.Draw( im )
+		font = ImageFont.truetype( "Arial.ttf", fsize )
+		draw.text( ( 10, 10 ), lines[0], font=font, fill="white" )
+
+		if len( lines ) > 1:
+			draw.text( ( 10, height - 10 - fsize ), lines[-1], font=font )
+
+		im.save( "tmp.jpg" )
 
 		response = cStringIO.StringIO()
 
 		c = pycurl.Curl()
+
 		values = [
-			("key", "8d4b1396c3d2a3712b299594600e0068"),
-			("image", (c.FORM_FILE, "tmp.jpg"))
+			( "key", self.imgur_token ),
+			( "image", ( c.FORM_FILE, "tmp.jpg" ) )
 		]
-		c.setopt(c.URL, "http://api.imgur.com/2/upload.json")
-		c.setopt(c.HTTPPOST, values)
-		c.setopt(c.WRITEFUNCTION, response.write)
+		
+		c.setopt( c.URL, "http://api.imgur.com/2/upload.json" )
+		c.setopt( c.HTTPPOST, values )
+		c.setopt( c.WRITEFUNCTION, response.write )
 		c.perform()
 		c.close()
-		ret = json.loads(response.getvalue())
-		return str(ret['upload']['links']['original'])
+		ret = json.loads( response.getvalue() )
+		return str( ret[ 'upload' ][ 'links' ][ 'original' ] )
 
 		
 	def getsentence(self, msg):
@@ -143,25 +139,35 @@ class twistedBot(irc.IRCClient):
 			return msg
 
 
-class twistedBotFactory(protocol.ClientFactory):
+class twistedBotFactory( protocol.ClientFactory ):
 	protocol = twistedBot
 
-	def __init__( self ):
-		config = ConfigParser.ConfigParser()
-		config.read( "bot.config" )
+	def __init__( self, config ):
 		self.channel = config.get( "IRC", "channel" )
 		self.nickname = config.get( "IRC", "nickname" )
+
 		self.chain_length = config.getint( "Markov", "chain_length" )
 		self.chattiness = config.getfloat( "Markov", "chattiness" )
 		self.max_words = config.getint( "Markov", "max_words" )
-		self.api = twitter.Api(config.get('OAuth','consumer_key'), config.get('OAuth','consumer_secret'), config.get('OAuth','access_token_key'), config.get('OAuth', 'access_token_secret'))
+
+		self.base = config.get( "Twitter", "base_url" )
+		self.api = twitter.Api( 
+								config.get( "Twitter", "consumer_key" ), 
+								config.get( "Twitter", "consumer_secret" ), 
+								config.get( "Twitter", "access_token_key" ), 
+								config.get( "Twitter", "access_token_secret" )
+							  )
+
+		self.imgur_token = config.get( "Kittify", "imgur_token" );
+
+		self.config = config
 	
-	def clientConnectionLost(self, connector, reason):
-		print "Lost connection "+str(reason)+", reconnection"
+	def clientConnectionLost( self, connector, reason ):
+		print "Lost connection " + str( reason ) + ", reconnection"
 		connector.connect()
 
 	def clientConnectionFailed(self, connector, reason):
-		print "Could not connect: "+str(reason)
+		print "Could not connect: " + str( reason )
 
 if __name__ == "__main__":
 	config = ConfigParser.ConfigParser()
@@ -180,5 +186,5 @@ if __name__ == "__main__":
 		print "brain loaded"
 		f.close()
 
-	reactor.connectTCP( server, port, twistedBotFactory() )
+	reactor.connectTCP( server, port, twistedBotFactory( config ) )
 	reactor.run()
