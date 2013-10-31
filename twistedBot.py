3af1dc117b5124730491b2d1ec24361205f2cea9
Stephen Bryant <stephen@stephenbryant.net>
2013-10-31 19:16:22 -0400

    Added better handling of multiple channels.

--- twistedBot.py
+++ twistedBot.py
@@ -14,6 +14,12 @@ class twistedBot( irc.IRCClient ):
 	def __init__( self ):
 		lastMsg = ""
 		self.loadBlacklist()
+		self.channels = []
+
+		channels = config.get( "IRC", "channels" )
+
+		for channel in channels.split( ',' ):
+			self.channels.append( channel.strip() )
 
 	def _get_nickname( self ):
 		return self.factory.nickname
@@ -21,8 +27,10 @@ class twistedBot( irc.IRCClient ):
 	nickname = property( _get_nickname )
 
 	def signedOn( self ):
-		self.join( self.factory.channel )
 		print "Signed on as " + self.nickname
+
+		for channel in self.channels:
+			self.join( channel )
 
 		self.conn = sqlite3.connect( self.factory.config.get( "General", "database_path" ) )
 		self.cursor = self.conn.cursor()
@@ -36,7 +44,12 @@ class twistedBot( irc.IRCClient ):
 
 		msgFrom = user.split( '!', 1 )[0]
 
-		self.logMessage( msgFrom, channel, msg, 'ACTION' )
+		blacklisted = 0
+
+	 	if msgFrom in self.blacklist:
+			blacklisted = 1
+
+		self.logMessage( msgFrom, channel, msg, 'ACTION', blacklisted )
 
 	def privmsg( self, user, channel, msg ):
 		if not user:
@@ -46,10 +59,12 @@ class twistedBot( irc.IRCClient ):
 
 		msgFrom = user.split( '!', 1 )[0]
 
-	 	if msgFrom in self.blacklist:
-			return
+		blacklisted = 0
 
-		self.logMessage( msgFrom, channel, msg, 'PRIVMSG' )
+	 	if msgFrom in self.blacklist:
+			blacklisted = 1
+
+		self.logMessage( msgFrom, channel, msg, 'PRIVMSG', blacklisted )
 
 		if msg.startswith( self.nickname ):
 			reply = self.parseMsg( " ".join( msg.split( " " )[1:] ), channel, msgFrom )
@@ -57,10 +72,10 @@ class twistedBot( irc.IRCClient ):
 		if reply != None and len( reply ) > 0:
 			replyMsg = "%s: %s" % ( msgFrom, reply )
 			self.msg( channel, replyMsg )
-			self.logMessage( nickname, channel, replyMsg )
+			self.logMessage( self.nickname, channel, replyMsg, "PRIVMSG", 0 )
 
-	def logMessage( self, user, channel, msg, type ):
-		self.cursor.execute( u"INSERT INTO log VALUES (?,?,?,?,?)", ( channel.decode('utf-8'), user.decode('utf-8'), msg.decode('utf-8'), int( time.time() ), type.decode('utf-8') ) )
+	def logMessage( self, user, channel, msg, type, blacklisted ):
+		self.cursor.execute( u"INSERT INTO log VALUES (?,?,?,?,?,?)", ( channel.decode('utf-8'), user.decode('utf-8'), msg.decode('utf-8'), int( time.time() ), type.decode('utf-8'), blacklisted ) )
 		self.conn.commit()
 
 	def parseMsg( self, msg, channel, user ): 
@@ -69,7 +84,7 @@ class twistedBot( irc.IRCClient ):
 			return self.factory.config.get( "General", "source_url" )
 
 		elif msg.startswith( "help" ):
-			return "Logs the channel. View at http://lug.fltt.us"
+			return "Logs the channel, view at http://lug.fltt.us. To be excluded from public logging, use the 'blacklist' command."
 
 		elif msg.startswith( "die" ):
  			if user == self.factory.owner:
@@ -81,7 +96,10 @@ class twistedBot( irc.IRCClient ):
 			if user == self.factory.owner:
 				channels = msg.split( " " )[1:]
 				for chan in channels:
+					self.channels.remove( chan )
 					self.leave( chan )
+
+				self.SaveChannels()
 			else:
 				self.msg( channel, "%s: Only owner can do that." % ( user, ) )
 	
@@ -89,7 +107,10 @@ class twistedBot( irc.IRCClient ):
 			if user == self.factory.owner:
 				channels = msg.split( " " )[1:]
 				for chan in channels:
+					self.channels.append( chan )
 					self.join( chan )
+
+				self.SaveChannels()
 			else:
 				self.msg( channel, "%s: Only owner can do that." % ( user, ) )
 		
@@ -101,7 +122,9 @@ class twistedBot( irc.IRCClient ):
 
 				self.msg( channel, "Added %s to blacklist" % ( ", ".join( nicks ), ) )
 			else:
-				self.msg( channel, "%s: Only owner can do that." % ( user, ) )
+				self.blacklist.append( user )
+				self.msg( channel, "Added %s to blacklist" % ( user, ) )
+				#self.msg( channel, "%s: Only owner can do that." % ( user, ) )
 
 		elif msg.startswith( "whitelist" ):
 			if user == self.factory.owner:
@@ -111,7 +134,9 @@ class twistedBot( irc.IRCClient ):
 
 				self.msg( channel, "Removed %s from blacklist" % ( ", ".join( nicks ), ) )
 			else:
-				self.msg( channel, "%s: Only owner can do that." % ( user, ) )
+				self.blacklist.remove( user )
+				self.msg( channel, "Removed %s from blacklist" % ( user, ) )
+				#self.msg( channel, "%s: Only owner can do that." % ( user, ) )
 		
 		else:
 			return ""
@@ -131,13 +156,19 @@ class twistedBot( irc.IRCClient ):
 		self.blacklist = json.load( blacklistFile )
 		blacklistFile.close()
 
+	def SaveChannels( self ):
+		channelString = ",".join( self.channels )
+		config.set( "IRC", "channels", channelString )
+
+		with open( "bot.config", "wb" ) as configfile:
+			config.write( configfile )
+
 
 class twistedBotFactory( protocol.ClientFactory ):
 	protocol = twistedBot
 	isQuitting = False
 
 	def __init__( self, config ):
-		self.channel = config.get( "IRC", "channel" )
 		self.nickname = config.get( "IRC", "nickname" )
 
 		self.source_url = config.get( "General", "source_url" )
